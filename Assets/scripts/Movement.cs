a7e7826a1c4e09b25b287b4c2e6cbacf224c2d33
Whopper <116588893+Whopper48474@users.noreply.github.com>
2023-03-09 21:50:11 +0100

    Add files via upload

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+       if (Input.GetKey(KeyCode.A))
+       {
+           transform.Rotate(Vector3.forward);
+       }
+       else if (Input.GetKey(KeyCode.D))
+       {
+         transform.Rotate(-Vector3.forward);
+       }
+    }
+}
